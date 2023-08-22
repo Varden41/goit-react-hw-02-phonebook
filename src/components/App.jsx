bd11c5bf5304310c638536efbcac45ebb9455700
Varden41 <arturpavlenko14@gmail.com>
2023-08-22 16:42:49 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
-class App extends Component {
-  state = { contacts: [], name: '', number: '' };
+// import {} from './ContactsSection';
+import ContactForm from './PhonebookSection';
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({
-      [name]: value,
-    });
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log(this.state);
+  dataHandler = data => {
+    console.log(data);
   };
 
   render() {
@@ -27,33 +30,9 @@ class App extends Component {
           color: '#010101',
         }}
       >
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name
-            <input
-              value={this.state.name}
-              onChange={this.handleChange}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-          <label>
-            Number
-            <input
-              value={this.state.number}
-              onChange={this.handleChange}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </label>
-          <button type="submit">Add contact</button>
-        </form>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.dataHandler} />
+        <h2>Contacts</h2>
       </div>
     );
   }
